3bffcf97d122b1f40cfbfdc5d0b80cdef391b371
enmvnuel <cristopherariasp@icloud.com>
2025-09-06 16:56:58 -0500

    first commit

---
+++ rsbuild.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+
+export default defineConfig({
+    plugins: [pluginReact()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["postcss-loader"],
+                type: "css",
+            },
+            // ...
+        ],
+    },
+    output: {
+        distPath: {
+            root: "dist", // Carpeta de salida
+        },
+    },
+});
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <body className="bg-gray-400 text-white justify-center items-center min-h-screen flex">
+                <h1 className="text-8xl font-bold underline">Hello world!</h1>
+            </body>
+        </>
+    );
+}
+
+export default App;
